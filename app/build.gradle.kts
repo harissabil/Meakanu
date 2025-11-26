ef57e37357a3b45fcbc528444a4531b14bcf7680
Haris <harissabil04@gmail.com>
2025-11-26 15:10:57 +0700

    Update compileSdk, targetSdk, versionCode, and versionName; refactor local properties loading and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -7,43 +9,37 @@ plugins {
 
 android {
     namespace = "com.harissabil.meakanu"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.harissabil.meakanu"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 22
-        versionName = "1.0.21"
-        ndkVersion = "26.1.10909125"
+        targetSdk = 36
+        versionCode = 24
+        versionName = "1.1.0"
+        ndkVersion = "29.0.14206865"
         ndk {
             debugSymbolLevel = "FULL"
         }
 
-        val baseUrlTrefle: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("BASE_URL_TREFLE")
-        val apiKeyTrefle: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("API_KEY_TREFLE")
-        val baseUrlPlantnet: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("BASE_URL_PLANTNET")
-        val apiKeyPlantnet: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("API_KEY_PLANTNET")
-        val baseUrlNews: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("BASE_URL_NEWS")
-        val apiKeyNews: String =
-            com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(
-                rootDir
-            ).getProperty("API_KEY_NEWS")
+
+        val localProperties = Properties().apply {
+            val file = rootProject.file("local.properties")
+            if (file.exists()) {
+                load(file.inputStream())
+            }
+        }
+
+        fun getLocalProperty(name: String): String =
+            localProperties.getProperty(name)
+                ?: error("Property '$name' not found in local.properties")
+
+        val baseUrlTrefle = getLocalProperty("BASE_URL_TREFLE")
+        val apiKeyTrefle = getLocalProperty("API_KEY_TREFLE")
+        val baseUrlPlantnet = getLocalProperty("BASE_URL_PLANTNET")
+        val apiKeyPlantnet = getLocalProperty("API_KEY_PLANTNET")
+        val baseUrlNews = getLocalProperty("BASE_URL_NEWS")
+        val apiKeyNews = getLocalProperty("API_KEY_NEWS")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_URL_TREFLE", baseUrlTrefle)
@@ -85,60 +81,61 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.6")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
 
     // Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
     implementation("jp.wasabeef:glide-transformations:4.3.0")
     //noinspection KaptUsageInsteadOfKsp
-    kapt("com.github.bumptech.glide:compiler:4.16.0")
-    implementation("com.github.bumptech.glide:annotations:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:5.0.5")
+    implementation("com.github.bumptech.glide:annotations:5.0.5")
 
     // KTX
-    implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.room:room-ktx:2.8.4")
+    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.8.4")
+    //noinspection KaptUsageInsteadOfKsp
+    kapt("androidx.room:room-compiler:2.8.4")
 
     // Coil
-    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil:2.7.0")
 
     // Spinner
     implementation("com.github.skydoves:powerspinner:1.2.7")
 
     // Splashscreen
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-splashscreen:1.2.0")
 
     // Paging 3
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.6")
 
     // uCrop
-    implementation("com.github.Frosch2010:uCrop-n-Edit:3.0.4")
+    implementation("com.github.jens-muenker:uCrop-n-Edit:4.0.2")
 
     // Browser
-    implementation("androidx.browser:browser:1.8.0")
+    implementation("androidx.browser:browser:1.9.0")
 }
